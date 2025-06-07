07f38a305d8cf3afdc313a1d3c167581bbdcc3c2
scaliann <mishah238@gmail.com>
2025-06-07 14:53:41 +0300

    Fix message_formatter

--- api_service/src/posts/models.py
+++ api_service/src/posts/models.py
@@ -1,10 +1,15 @@
 from sqlalchemy import String, Text, DateTime
 from src.models import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
+
+
+def utc_plus_3() -> datetime:
+    """Возвращает текущее время + 3 ч, привязанное к UTC+3."""
+    return datetime.now(timezone.utc) + timedelta(hours=3)
 
 
 class Post(Base):
     title: Mapped[str] = mapped_column(String(255))
     text: Mapped[str] = mapped_column(Text)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=utc_plus_3)
--- bot_service/src/message_formatter.py
+++ bot_service/src/message_formatter.py
@@ -6,4 +6,8 @@ class MessageFormatter:
     def format_post(post: Post) -> str:
         formatted_date = post.created_at.strftime("%Y-%m-%d %H:%M:%S")
 
-        return f"📝 *{post.title}*\n\n" f"{post.text}\n\n" f"📅 Created: {formatted_date}"
+        return (
+            f"📝 *Заголовок: {post.title}*\n\n"
+            f"Текст: {post.text}\n\n"
+            f"📅 Время создания: {formatted_date}"
+        )
