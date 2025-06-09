73ea9f7147471d6be8e52c0fc8c385648fd34ab3
scaliann <mishah238@gmail.com>
2025-06-09 15:08:27 +0300

    fix .env+dockerfile

--- api_service/src/config.py
+++ api_service/src/config.py
@@ -3,10 +3,16 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
 
     class Config:
         env_file = ".env"
 
 
 settings = Settings()
+
+
+def get_auth_data():
+    return {"secret_key": settings.SECRET_KEY, "algorithm": settings.ALGORITHM}
--- api_service/src/main.py
+++ api_service/src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from src.models import Base
 from src.database import engine
 from src.posts.routes import router as posts_router
+from src.users.routes import router as users_router
 
 
 @asynccontextmanager
@@ -18,7 +20,24 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Posts API", lifespan=lifespan)
 
+
+origins = [
+    "http://localhost",
+    "http://localhost:5500",
+    "http://127.0.0.1",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(posts_router)
+app.include_router(users_router)
 
 
 @app.get("/")
